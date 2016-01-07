8fb74591164f5cc3b06ba076c367159a140b9084
geziene <glavely89@yahoo.com>
2016-01-07 11:44:35 +0100

    TEST

--- app/src/main/java/com/example/danielbordig/studyssmartfragment/SGM_frag.java
+++ app/src/main/java/com/example/danielbordig/studyssmartfragment/SGM_frag.java
@@ -1,6 +1,7 @@
 package com.example.danielbordig.studyssmartfragment;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.DrawableRes;
 import android.view.LayoutInflater;
@@ -24,11 +25,22 @@ public class SGM_frag extends Fragment implements AdapterView.OnItemClickListene
 
     TextView Header, UnderHeader;
     ListView GroupList;
-   // ArrayList<Integer> Groups = new ArrayList<Integer>();
     int[] Groups = {R.drawable.bmp, R.drawable.ns,};
     String[] Emne = {"", "",};
 
-
+    String[] homeworkoverview = new String[]{
+            "This homework is awesome, lets start.\nBook: Naruto 1-72",
+            "This homework sucks, I don't want to study today.\nBook: Fifty Shades of Grey",
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Itme 4",
+            "Item 5",
+            "Item 6",
+            "Itme 7",
+            "Item 8",
+            "Item 9",
+    };
 
     public SGM_frag() {
         // Required empty public constructor
@@ -85,11 +97,29 @@ public class SGM_frag extends Fragment implements AdapterView.OnItemClickListene
 
     @Override
     public void onClick(View v) {
+//        if(v==a){
+//            b.setTextColor(Color.BLACK);
+//            b.setBackgroundColor(Color.WHITE);
+//            b.setClickable(true);
+//    }
+//        if(v==b) {
+//            a.setTextColor(Color.BLACK);
+//            a.setBackgroundColor(Color.WHITE);
+//            a.setClickable(true);
 
     }
 
+
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String hmover = homeworkoverview[position];
+
+        Intent intentBundle = new Intent(getActivity(), HomeworkDetails.class);
+        Bundle bundle = new Bundle();
+
+        bundle.putString("detail", hmover);
+        intentBundle.putExtra("detailBundle", bundle);
+        startActivity(new Intent(intentBundle));
 
     }
 }
