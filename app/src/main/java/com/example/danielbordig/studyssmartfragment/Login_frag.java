102d96ed2f2b99d82de74d5310f16c86a3377218
Nicklas Jensen <nick4000@sol.dk>
2016-01-12 11:45:10 +0100

    Login har forbindelse til Firebase

--- app/src/main/java/com/example/danielbordig/studyssmartfragment/Login_frag.java
+++ app/src/main/java/com/example/danielbordig/studyssmartfragment/Login_frag.java
@@ -12,13 +12,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 public class Login_frag extends Fragment implements View.OnClickListener {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
     private String mParam2;
-
 
     ImageView logoStudySmart;
     TextView header;
@@ -35,9 +39,15 @@ public class Login_frag extends Fragment implements View.OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //FireBase
+        Firebase myFirebaseRef;
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+
         }
     }
 
@@ -68,16 +78,47 @@ public class Login_frag extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        String nameString = name.getText().toString().trim();
-        String passwordString = password.getText().toString().trim();
+
+        final String nameString = name.getText().toString().trim();
+        final String passwordString = password.getText().toString().trim();
         String login = "";
+
+        Firebase LoginFirebase = new Firebase("https://studysmart.firebaseio.com/CBS/Logins/");
+        LoginFirebase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if(nameString.isEmpty() || passwordString.isEmpty()){
+                    Toast.makeText(getActivity(), "Fill out all fields", Toast.LENGTH_LONG).show();
+                }
+                else if((snapshot.child("Userid/" + nameString).getValue() == null)){
+                    Toast.makeText(getActivity(), "User doesn't exist", Toast.LENGTH_LONG).show();
+                }
+                else if(!(snapshot.child("Password/" + nameString + "/Kodeord").getValue().equals(passwordString))){
+                    Toast.makeText(getActivity(), "Wrong Password", Toast.LENGTH_LONG).show();
+                }
+                else if(snapshot.child("Userid/" + nameString + "/Active").getValue().equals("Yes") && snapshot.child("Password/" + nameString + "/Kodeord").getValue().equals(passwordString)) {
+                        getFragmentManager().beginTransaction().replace(R.id.mainFrame, fragment_hwc).commit();
+                    } else {
+                        Toast.makeText(getActivity(), "Username and/or Password is wrong", Toast.LENGTH_LONG).show();
+                    }
+                }
+
+
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
+
+        /*
         if(nameString.isEmpty() || passwordString.isEmpty()){
             Toast.makeText(getActivity(), "Fill out all fields", Toast.LENGTH_LONG).show();
         }
         else {
             login = stuDAO.Login(nameString, passwordString);
         }
-        if (login.equals("true")) {
+        if(snapshot.child("Active").getValue().equals("Yes") && snapshot.child("Kodeord").getValue().equals(Kodeord)) {
             getFragmentManager().beginTransaction().replace(R.id.mainFrame, fragment_hwc).commit();
         }
         else if(login.equals("trueName")){
@@ -86,6 +127,7 @@ public class Login_frag extends Fragment implements View.OnClickListener {
         else if(login.equals("falseUser")){
             Toast.makeText(getActivity(), "Wrong username", Toast.LENGTH_LONG).show();
         }
+        */
     }
 
 }
